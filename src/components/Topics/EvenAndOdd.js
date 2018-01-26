cfdfdf2acb2919ed835ba6d05c6d1d53983cde5b
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-01-26 14:18:27 -0700

    almost done with EvenAndOdds component

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+
+
+class EvenAndOdd extends Component {
+   render(){
+       return(
+
+        <div className="puzzleBox evenAndOddPB">
+
+            <input className="inputLine"></input>
+
+            <button className="confirmationButton"></button>
+
+            <span className="resultBox"></span>
+
+            <span className="resultBox"></span>
+
+        </div>
+       )}
+}
+
+export default EvenAndOdd
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FilterObject extends Component{
+
+    render(){
+        return(
+
+            <p>FilterObject Component</p>
+
+        
+        )
+    }
+}
+
+export default FilterObject
