95cd10ec9f759d3bfc211c992bce229bc084d789
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-01-27 15:49:48 -0700

    almost done

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
 class FilterString extends Component{
+    constructor(){
+        super()
+        this.state = {
+            unFilteredArray: ['josh', 'annie', 'jeff', 'jake', 'lora', 'alexa', 'bird'],
+            userInput: '',
+            filterArray: []
+        }
+    }
+
+    solveToy(prop){
+        var newArray = []
+        var arrayOfStrings = this.state.unFilteredArray;
+        for(let i =0; i < arrayOfStrings.length; i++){
+            if(arrayOfStrings[i].match(prop)){
+                newArray.push(prop)
+            }
+        }
+        this.setState({
+            filterArray: newArray
+        })
+    }
+
     render(){
         return (
-            <p>FilterString Component</p>
+            <div className="puzzleBox filterStringPB">
+                <h4>Filter String</h4>
+                <span className="puzzleText">{JSON.stringify(this.state.unFilteredArray)}</span>
+                <input className='inputLine' onChange={(e) => this.setState({userInput: e.target.value})}/>
+                <button className='confirmationButton' onClick={this.solveToy(this.state.userInput)}></button>
+                <span className='resultsBox filterStringRB'>{JSON.stringify(this.state.filterArray)}</span>
+            </div>
         )
     }
 }
